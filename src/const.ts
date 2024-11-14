c013a710f5bc9871ff9aa5f29958543db536058a
zhouzhihong <zhouzhihong@ijunhai.com>
2024-11-14 18:05:42 +0800

    Add files

---
+++ src/const.ts
@@ -0,0 +1,81 @@
+export const WEB_SITE_NAME = '个人站点';
+export const ICP_NUM = '备案号';
+
+
+type tour = {
+    face: string,
+    title: string,
+    year: string,
+    description: string
+};
+
+export const TOURS: Array<tour> = [
+    {
+        face: "https://images.unsplash.com/photo-1515700281303-5a0a73d9c584?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=elizeu-dias-ibBMmZjxKSo-unsplash.jpg&w=640",
+        year: "2023",
+        title: "南美洲风情之旅",
+        description: "我来到了热情似火的南美洲，在巴西里约热内卢，基督像张开双臂俯瞰着这座充满活力的城市。科帕卡巴纳海滩上，人们尽情享受阳光和沙滩，足球在人群中穿梭，桑巴音乐似乎在空气中流淌，处处彰显着巴西人的热情。而在秘鲁，马丘比丘这座古老的印加帝国遗址让我深深着迷。它坐落在群山之间，云雾缭绕下更显神秘。古老的石墙、狭窄的街道、宏伟的建筑，无不诉说着曾经辉煌的文明，我仿佛能看到古代印加人的生活场景，感受到他们的智慧和勤劳。"
+    },
+    {
+        face: "https://images.unsplash.com/photo-1517846875602-2371528547d4?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=sutirta-budiman-89IBtfoz3Vw-unsplash.jpg&w=640",
+        year: "2022",
+        title: "非洲探秘之旅",
+        description: "这一年，我怀着对原始神秘大陆的向往，开启了非洲之旅。在肯尼亚，我深入马赛马拉国家保护区，见证了举世闻名的动物大迁徙。那浩浩荡荡的角马群如奔腾的河流，它们跨越马拉河的场景惊心动魄，河中潜伏的鳄鱼虎视眈眈，生死瞬间在眼前上演，这是大自然最残酷也最壮丽的画卷。除了大迁徙，保护区内的狮子、猎豹、大象等各种珍稀动物也让我大开眼界。它们在广袤的草原上自由生活，每一个姿态都展现出生命的力量。在南非开普敦，我登上桌山，从山顶俯瞰，大西洋和印度洋在这里交汇，那湛蓝的海水、翻滚的海浪和远处的山峦构成了一幅气势磅礴的画面。开普敦的城市风光也别具一格，色彩斑斓的房屋点缀在海边，充满了活力。"
+    },
+    {
+        face: "https://images.unsplash.com/photo-1673627114526-b83c09d3a760?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=vaskar-sam-ZHh4z7-K4tI-unsplash.jpg&w=640",
+        year: "2021",
+        title: "东南亚海滨之旅",
+        description: "冬天，我来到了泰国普吉岛，享受阳光沙滩的悠闲时光。白色的沙滩细腻柔软，蓝色的大海一望无际。我躺在沙滩椅上，沐浴着阳光，听着海浪的声音，感受着大自然的美好。普吉岛的水上活动丰富多彩，我尝试了潜水和浮潜，看到了五彩斑斓的海底世界。在马来西亚兰卡威，美丽的海岛风光让我流连忘返。天空之桥的惊险刺激让我心跳加速，珍南海滩的日落美景让我陶醉其中。"
+    },
+    {
+        face: "https://images.unsplash.com/photo-1729284439331-0afedb2cb5ea?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=rubina-ajdary-eTTgAAVEJl4-unsplash.jpg&w=640",
+        year: "2020",
+        title: "国内山水之旅",
+        description: "由于特殊情况，我的旅行脚步留在了国内。我走进了桂林，领略了甲天下的山水风光。漓江的水清澈见底，两岸的山峰形态各异。我乘坐竹筏顺流而下，仿佛置身于一幅美丽的山水画卷之中。阳朔的西街充满了文艺气息，古老的建筑与现代的商业完美融合。在张家界，奇峰异石让人叹为观止。天子山的云海如仙境般美丽，袁家界的阿凡达取景地更是让我仿佛进入了一个奇幻的世界。"
+    },
+    {
+        face: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=chris-karidis-nnzkZNYWHaU-unsplash.jpg&w=640",
+        year: "2019",
+        title: "欧洲文化之旅",
+        description: "夏天，我飞往欧洲，感受古老而璀璨的文化魅力。在法国巴黎，埃菲尔铁塔高耸入云，塞纳河静静流淌。我登上埃菲尔铁塔，俯瞰整个巴黎的美景，感受这座浪漫之都的魅力。卢浮宫的艺术珍品让我惊叹不已，蒙娜丽莎的微笑更是让我沉醉其中。在意大利罗马，古老的斗兽场诉说着历史的沧桑。我漫步在古罗马广场，仿佛能听到千年前的呐喊声。万神殿的宏伟建筑让人感受到古罗马人的智慧和创造力。"
+    },
+    {
+        face: "https://images.unsplash.com/photo-1723569199338-34d3e73e2499?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=stephen-h-Ox5h3uz9t6o-unsplash.jpg&w=640",
+        year: "2018",
+        title: "日本赏樱之旅",
+        description: "春天，我踏上了日本的土地，开启了一场浪漫的赏樱之旅。在京都，古老的寺庙与绚烂的樱花相互映衬，仿佛穿越回了过去。清水寺的红色建筑在粉色樱花的簇拥下，显得格外庄重而美丽。漫步在哲学之道，两岸的樱花树如粉色的云朵般绽放，花瓣随风飘落，如同梦幻的仙境。在奈良，可爱的小鹿与樱花共同构成了一幅和谐的画面。小鹿们在樱花树下嬉戏玩耍，时而向游客讨要食物，时而安静地享受着春天的气息。"
+    }
+];
+
+
+export const ALBUM_NAME_ANIMAL_WORLD = 'animal-world'
+export const ALBUM_NAME_ENCHANTED_FOREST = 'enchanted-forest'
+
+// type album = {
+//     title: string,
+//     face: string
+// }
+
+export const ALBUMS = {
+    'animal-world': {
+        title: "开启奇妙的动物王国之旅",
+        description: "汇聚了大自然神奇与美妙的视觉盛宴，它宛如一扇通往神秘动物王国的大门，引领读者走进一个充满生机与魅力的世界。",
+        images: [
+            {
+                url: "",
+                desc: ""
+            }
+        ]
+    },
+    'enchanted-forest': {
+        title: "踏入奇幻之境的神秘邀约",
+        description: "宛如一把神奇的钥匙，为你开启一扇通往充满神秘与魅力世界的大门。在这个世界里，每一页都弥漫着魔法的气息，而那巨大的树木无疑是这片魔法森林的灵魂所在",
+        images: [
+            {
+                url: "",
+                desc: ""
+            }
+        ]
+    }
+}
